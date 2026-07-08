555d973623f99a50a7e122a3a448426bdbd3e245
Prabal Pophaley <ppophaley@gmail.com>
2026-07-08 14:35:21 +0530

    Add controller and db for user-service.

---
+++ order-service/src/main/java/com/apigatewayplatform/orderservice/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.apigatewayplatform.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from Order Service";
+    }
+}
